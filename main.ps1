a79f8514f69cb71d453e0a2bb646509e62821a51
Invaliff <46950359+Invaliff@users.noreply.github.com>
2023-12-29 15:51:56 -0600

    Only alerting once per file

--- main.ps1
+++ main.ps1
@@ -41,12 +41,13 @@ if ($response -eq "A".ToUpper()) {
 
 elseif ($response -eq "B".ToUpper()) {
 
-    $fileHashDictonary = @{}
+    $fileHashDictionary = @{}
+    $AlertDictionary = @{}
     #load file|hash from baseline.txt and store in dictonary
     $filePathsAndHashes = Get-Content -Path .\baseline.txt
 
     foreach ($f in $filePathsAndHashes) {
-        $fileHashDictonary.add($f.Split("|")[0].Trim(), $f.Split("|")[1].Trim())
+        $fileHashDictionary.add($f.Split("|")[0].Trim(), $f.Split("|")[1].Trim())
     }
 
     #Begin continously
@@ -59,30 +60,38 @@ elseif ($response -eq "B".ToUpper()) {
         foreach ($f in $files) {
             $hash = Calculate-File-Hash $f.FullName
             #notify if a new file has been created
-            if ($null -eq $fileHashDictonary[$hash.Path]) {
+            if ($null -eq $fileHashDictionary[$hash.Path]) {
                 # A new file has been created
                 Write-Host "$($hash.Path) has been created!" -ForegroundColor Green
+
+                # Adds to the directory to avoid repeat hits
+                $fileHashDictionary.add($hash.Path, $Hash.Hash)
             }
             else {
                 # notify if a new file has been changed
-                if ($fileHashDictonary[$hash.Path] -eq $($hash.Hash)) {
+                if ($fileHashDictionary[$hash.Path] -eq $($hash.Hash)) {
                     # Path has not changed
                 }
                 else {
                     # file has been comprimised!
                     Write-Host "$($hash.Path) Has been changed!" -ForegroundColor Yellow
+
+                    # Adds to the directory to avoid repeat hits
+                    $fileHashDictionary[$hash.Path] = $hash.Hash
                 }
             }
 
             # Notify if a file has been deleted
             
-            foreach ($key in $fileHashDictonary.Keys) {
+            foreach ($key in $fileHashDictionary.Keys) {
                 $baselineFileStillExists = Test-Path -Path $key
-                if (-Not $baselineFileStillExists) {
+                if ((-Not $baselineFileStillExists) -and ($null -eq $AlertDictionary[$key])) {
                     #One of the baseline files have been deleted notify user
                     Write-Host "$($key) has been deleted!" -ForegroundColor DarkMagenta
+
+                    $AlertDictionary[$key] = $fileHashDictionary[$key]
                 }
-            }
-        } # end of for each
+            } # stopping looping through keys
+        } # end of loop through files
     } # End of while
 } # end of choice B
