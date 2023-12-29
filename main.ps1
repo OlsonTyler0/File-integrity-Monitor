afab850ba282ee36379e1d36c3cc191d67fb9c60
Invaliff <46950359+Invaliff@users.noreply.github.com>
2023-12-29 01:01:35 -0600

    Update main.ps1 with code
    Needs additional work on ensuring the program works

--- main.ps1
+++ main.ps1
@@ -1,5 +1,3 @@
-
-
 Write-Host ""
 Write-Host "What would you like to do?"
 Write-Host "A) Collect new baseline"
@@ -24,7 +22,7 @@ function Erase-Baseline-If-Already-Exists() {
 }
 
 if ($response -eq "A".ToUpper()) {
-    #Delete baseline.txt if it exists
+    #Delete baseline.txt if it already exists
     Erase-Baseline-If-Already-Exists
     
     # Calculate hash from the target files and store in baseline
@@ -38,8 +36,52 @@ if ($response -eq "A".ToUpper()) {
         $hash = Calculate-File-Hash $f.FullName
         "$($hash.Path) | $($hash.hash)" | Out-File -FilePath .\baseline.txt -Append
     }
-}
+} # End of choice A
+
 elseif ($response -eq "B".ToUpper()) {
-    <# Begin monitoring files with saved baseline #>
-    Write-Host "Read existing baseline.txt, start monitoring files." -ForegroundColor Yellow
-}
+
+    $fileHashDictonary = @{}
+    #load file|hash from baseline.txt and store in dictonary
+    $filePathesAndHashes = Get-Content -Path .\baseline.txt
+
+    foreach ($f in $filePathsAndHashes) {
+        $fileHashDictonary.add($f.Split("|")[0], $f.Split("|")[1])
+    }
+
+    #Begin continously
+    while ($true) {
+        Start-Sleep -Seconds 1
+        
+        $files = Get-ChildItem -Path .\files
+
+        # for each file, calculate the hash and write to baseline.txt
+        foreach ($f in $files) {
+            $hash = Calculate-File-Hash $f.FullName
+
+            #notify if a new file has been created
+            if ($null -eq $fileHashDirectory[$hash.Path]) {
+                # A new file has been created
+                Write-Host "$($hash.Path) has been created!" -ForegroundColor Green
+            }
+
+            # notify if a new file has been changed
+            if ($fileHashDictonary[$hash.Path] -eq $hash.Hash) {
+                # Path has not changed
+            }
+            else {
+                # file has been comprimised!
+                Write-Host "$($hash.Path) Has been changed!" -ForegroundColor Yellow
+            }
+
+            # Notify if a file has been deleted
+            
+            foreach ($key in $fileHashDictonary.Keys) {
+                $baselineFileStillExists = Test-Path -Path $key
+                if (-Not $baselineFileStillExists) {
+                    #One of the baseline files have been deleted notify user
+                    Write-Host "$($key) has been deleted!" -ForegroundColor DarkMagenta
+                }
+            }
+        } # end of for each
+    } # End of while
+} # end of choice B
